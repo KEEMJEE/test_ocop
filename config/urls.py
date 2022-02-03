ba97788d1b34aef218d216c00b8e6611adfa9f03
kee <itscloudy0218@gmail.com>
2022-02-03 10:13:13 +0900

    pybo

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('point/', include('point.urls')),
+    path('mypage/', include('mypage.urls')),
+    path('pybo/', include('pybo.urls')),
 ]
