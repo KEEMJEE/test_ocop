f62eca5fb2a150470c62a0abdbae6fb322f78d28
kee <itscloudy0218@gmail.com>
2022-02-04 13:18:36 +0900

    mypage3

--- pybo/urls.py
+++ pybo/urls.py
@@ -7,6 +7,14 @@ app_name = 'pybo'
 urlpatterns = [
     path('', views.pybo, name='pybo'),
     path('<int:question_id>/', views.detail, name='detail'),
+    path('question/create/', views.question_create, name = 'question_create'),
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
-    path('question/create/', views.question_create, name='question_create'),
+    # vote_views.py
+    path('vote/question/<int:question_id>/', views.vote_question, name='vote_question'),
+
+    #category
+    path('question/list/', views.pybo, name='pybo'),
+    path('question/list/<str:category_name>/', views.pybo, name='pybo'),
+    path('question/detail/<int:question_id>/', views.detail, name='detail'),
+    path('question/create/<str:category_name>/', views.question_create, name='question_create_category'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,17 +1,43 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Question, Answer
+from .models import Question, Answer, Category
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
+from django.db.models import Q, Count
 # from django.http import HttpResponse
 
 # Create your views here.
 
-def pybo(request):
+def pybo(request, category_name='carbon'):
     """
     pybo 목록 출력
     """
-    question_list = Question.objects.order_by('-create_date')
-    context = {'question_list' : question_list}
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+    so = request.GET.get('so', 'recent')  # 정렬기준
+
+    category_list = Category.objects.all()
+    category = get_object_or_404(Category, name=category_name)
+    question_list = Question.objects.filter(category=category)
+
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
+    # 조회, 검색
+    # question_list = Question.objects.order_by('-create_date')
+
+    # 페이징처리
+    paginator = Paginator(question_list, 5)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj, 'page': page, 'so': so,
+               'category_list': category_list, 'category': category}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
@@ -22,20 +48,23 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
-def question_create(request):
+def question_create(request, category_name):
     """
     pybo 질문등록
     """
+    category = Category.objects.get(name=category_name)
+
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
             question.create_date = timezone.now()
+            question.category = category
             question.save()
-            return redirect('pybo:pybo')
-    else:
+            return redirect(category)
+    else: # request.method == 'GET'
         form = QuestionForm()
-    context = {'form': form}
+    context = {'form': form, 'category':category }
     return render(request, 'pybo/question_form.html', context)
 
 def answer_create(request, question_id):
@@ -55,3 +84,14 @@ def answer_create(request, question_id):
         form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
+
+def vote_question(request, question_id):
+    """
+    pybo 질문추천등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    # if request.user == question.author:
+    #     messages.error(request, '본인이 작성한 글은 추천할수 없습니다')
+    # else:
+    question.voter.add(request.user)
+    return redirect('pybo:detail', question_id=question.id)
