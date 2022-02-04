d09dd0f3ab2aedd85ae0c4777b6f120999e46d97
jjw <jjw@mysite.com>
2022-02-04 13:15:30 +0900

    point

--- config/settings.py
+++ config/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'OCOP',
+    'point',
+    'pybo',
+    'mypage',
 
 ]
 
