7cc1ce6fc90e9062d999e1108f1bbd9c4ac7ca02
kee <itscloudy0218@gmail.com>
2022-02-07 09:46:53 +0900

    mypage3

--- pybo/views.py
+++ pybo/views.py
@@ -9,12 +9,13 @@ from django.db.models import Q, Count
 
 # Create your views here.
 
-def pybo(request, category_name='carbon'):
+def pybo(request, category_name='notice'):
     """
     pybo 목록 출력
     """
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
     so = request.GET.get('so', 'recent')  # 정렬기준
 
     category_list = Category.objects.all()
@@ -23,20 +24,27 @@ def pybo(request, category_name='carbon'):
 
     # 정렬
     if so == 'recommend':
-        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+        question_list = question_list.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
     elif so == 'popular':
-        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+        question_list = question_list.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
     else:  # recent
-        question_list = Question.objects.order_by('-create_date')
+        question_list = question_list.order_by('-create_date')
 
     # 조회, 검색
     # question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) #|  # 내용검색
+            # Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            # Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
 
     # 페이징처리
     paginator = Paginator(question_list, 5)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj, 'page': page, 'so': so,
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so,
                'category_list': category_list, 'category': category}
     return render(request, 'pybo/question_list.html', context)
 
